7c21dd6cbb754e5009508e2b98a72e5c2ae91b0f
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-06-17 15:58:16 -0700

    Create svg circles

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,27 @@
 // start slingin' some d3 here.
+
+//var boardWidth and height
+
+var enemyData = [];
+var numEnemies = 4;
+var boardWidth = 700;
+var boardHeight = 500;
+var enemyRadius = 50;
+
+for (var i = 0; i < numEnemies; i++) {
+  enemyData.push({x: (Math.random() * (boardWidth - 2 * enemyRadius) + enemyRadius) + 'px',
+                  y: (Math.random() * (boardHeight - 2 * enemyRadius) + enemyRadius) + 'px'});
+}
+
+var svg = d3.select('.board').append('svg')
+  .attr('width', boardWidth)
+  .attr('height', boardHeight)
+  .append('g');
+
+var enemies = d3.select('svg').selectAll('circle').data(enemyData);
+enemies.enter()
+  .append('circle')
+  .attr('r', enemyRadius)
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('fill', 'red');
